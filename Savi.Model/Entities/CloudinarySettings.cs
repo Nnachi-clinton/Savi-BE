aa3e14a6a3ef04eb39bc76115903621f4fbdefd4
Nnachi-clinton <clinton.amadi@decagon.dev>
2023-12-21 02:54:41 +0100

    cloudinary service implemented

---
+++ Savi.Model/Entities/CloudinarySettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Savi.Model.Entities
+{
+    public class CloudinarySettings
+    {
+        public string CloudName { get; set; } = string.Empty;
+        public string APIKey { get; set; } = string.Empty;
+        public string APISecret { get; set; } = string.Empty;
+    }
+}
