23457505713937aa5bef18e813d6752682932133
Nnachi-clinton <clinton.amadi@decagon.dev>
2024-01-07 22:13:32 +0100

    Implemented personal savings services

---
+++ Savi.Api/Controllers/SavingsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Savi.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SavingsController : ControllerBase
+    {
+    }
+}
